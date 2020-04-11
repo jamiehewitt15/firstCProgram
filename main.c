46f059872b53ddd6c46e81bc5b468f17aef89c99
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-11 17:39:10 +0300

    charachter scan

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main() {
 
+    char password[1];
+    printf("Type a  password: ");
+    scanf("%s", password);
+    printf("\nYour password is: %s\n", password);
 
     float x;
     printf("Type an float: ");
