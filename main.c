9d59206647765bd824f5c06423d7ce34627e0948
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-11 22:19:57 +0300

    outputting average of inputs

--- main.c
+++ main.c
@@ -7,22 +7,24 @@ int main() {
     int arrayEntries = 0;
     int arraySum = 0;
     int input;
-    do {
+
+    while(input != -1) {
         printf("Please enter input positive number: ");
         scanf("%d", &input);
         if (input > 0 && sizeof(input) <= 4){
             printf("You entered: %d\n", input);
             inputArray[arrayEntries] = input;
             arrayEntries++;
-        } else{
+        } else if (input <0 && input !=-1){
             printf("Sorry, you must enter input positive number.\n");
         }
-    } while(input != -1);
+    };
     for(int i=0; i < arrayEntries; i++)
         {
             arraySum = arraySum + inputArray[i];
         }
-        printf("The sum of all input numbers is: %\n", arraySum);
+        printf("The sum of all input numbers is: %d\n", arraySum);
+        printf("The average of all input numbers is: %d\n", arraySum/arrayEntries);
         printf("Thank you, goodbye!");
         return 0;
 }
