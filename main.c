498a9e0f128b6388dc72012c7316641af3d11b28
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-11 17:18:07 +0300

    scan for a float

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 int main() {
 
 
-    int x;
-    printf("Type an integer: ");
-    scanf("%d", &x);
-    printf("You typed the integer: %d\n", x);
+    float x;
+    printf("Type an float: ");
+    scanf("%f", &x);
+    printf("You typed the float: %f\n", x);
 
     return 0;
 }
