5a75fc57bfcfc03fca0cecbdc7ed7681803a2691
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-11 16:45:59 +0300

    adding initial files

---
+++ cmake-build-debug/CMakeFiles/question2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question2.dir/main.c.obj"
+  "libquestion2.dll.a"
+  "question2.exe"
+  "question2.exe.manifest"
+  "question2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/question2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,20 @@
+/* Jamie Hewitt - Question 2 */
+#include <stdio.h>
+
+int main() {
+    printf("Please enter a positive number: \n");
+    printf("My name is \'Jamie\'\n");
+    int age = 29;
+    printf("I am %d years old\n", age);
+    age = 35;
+    printf("I am %d years old\n", age);
+    printf("This is a backslash: \\\n");
+
+    float pi;
+    pi = 22.00 / 7.0;
+    printf("PI is: %f",pi);
+
+
+
+    return 0;
+}
