500815c8b52a68d61b7c81d747daf6a6200d0669
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-12 19:03:18 +0300

    making sure number is entered and not text

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 /* Jamie Hewitt - Question 2 */
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_ARRAY_SIZE 100
 
 int main() {
     int inputArray[MAX_ARRAY_SIZE];
     int arrayEntries = 0;
     int arraySum = 0;
-    int input;
-
+    char input[16];
+    char *p;
+    int  inputNumber;
+    
     while(input != -1) {
         printf("Please enter input positive number: ");
-        scanf("%d", &input);
-        if (input > 0 && sizeof(input) <= 4){
-            printf("You entered: %d\n", input);
-            inputArray[arrayEntries] = input;
-            arrayEntries++;
-        } else if (input <0 && input !=-1){
+        fgets(input,16,stdin);
+        inputNumber = strtol(input,&p,10);
+        if (p == input){
+            printf("Sorry, that is invalid input.\n");
+        }
+        else if(inputNumber <=0 && input !=-1){
             printf("Sorry, you must enter input positive number.\n");
+        }
+        else if (input > 0 && sizeof(inputNumber) <= 4){
+            printf("You entered: %d\n", inputNumber);
+            inputArray[arrayEntries] = inputNumber;
+            arrayEntries++;
         }
     };
     for(int i=0; i < arrayEntries; i++)
