bfea5ff256594d0b44a1bb8118422c5fb7a9d22a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-12 19:21:35 +0300

    correctly counting total

--- main.c
+++ main.c
@@ -11,17 +11,17 @@ int main() {
     char *p;
     int  inputNumber;
     
-    while(input != -1) {
+    while(inputNumber != -1) {
         printf("Please enter input positive number: ");
         fgets(input,16,stdin);
         inputNumber = strtol(input,&p,10);
         if (p == input){
             printf("Sorry, that is invalid input.\n");
         }
-        else if(inputNumber <=0 && input !=-1){
+        else if(inputNumber <=0 && inputNumber !=-1){
             printf("Sorry, you must enter input positive number.\n");
         }
-        else if (input > 0 && sizeof(inputNumber) <= 4){
+        else if (inputNumber > 0 ){
             printf("You entered: %d\n", inputNumber);
             inputArray[arrayEntries] = inputNumber;
             arrayEntries++;
@@ -29,8 +29,9 @@ int main() {
     };
     for(int i=0; i < arrayEntries; i++)
         {
+            printf("The sum of all input numbers is: %d\n", arraySum);
             arraySum = arraySum + inputArray[i];
-        }
+            }
         printf("The sum of all input numbers is: %d\n", arraySum);
         printf("The average of all input numbers is: %d\n", arraySum/arrayEntries);
         printf("Thank you, goodbye!");
